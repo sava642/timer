617e3afbb12c83d8d5a69d6be8e18d76360bf137
sava642 <101993338+sava642@users.noreply.github.com>
2022-04-02 09:50:15 +0200

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+
+import React from 'react';
+
+export default class App extends React.Component {
+  state = {
+    count: 0,
+    isCounting: false,
+  };
+  myTimer = null;
+  handleStart = () => {
+
+    this.myTimer = setInterval(() => (
+      this.setState({ count: this.state.count += 1 })
+
+    ), 1000)
+    this.setState({ isCounting: true })
+  }
+  handleStop = () => {
+    this.setState({ isCounting: false })
+    clearInterval(this.myTimer);
+  }
+  handleReset = () => {
+    localStorage.removeItem('timerStorage')
+    this.setState({ count: 0, isCounting: false })
+  }
+
+
+  componentDidMount() {
+    const tmrStrg = localStorage.getItem('timerStorage')
+    console.log("страница обновилась")
+    if (tmrStrg) {
+      this.setState({ count: this.state.count = +tmrStrg })
+    }
+  }
+
+
+  componentDidUpdate() {
+    localStorage.setItem('timerStorage', this.state.count)
+  }
+
+  componentWillUnmount() {
+    console.log("демонтирование")
+    clearInterval(this.myTimer);
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <h1>React Timer</h1>
+        <h3>{this.state.count}</h3>
+        {!this.state.isCounting ? (
+          <button onClick={this.handleStart}>Start</button>
+        ) : (
+          <button onClick={this.handleStop}>Stop</button>
+        )}
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
